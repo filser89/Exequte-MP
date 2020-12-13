7004dc88e23d6b0313a75310fc53ab8f7f44ec2b
Sergey Filippov <sergey.filippov.89@inbox.ru>
2020-12-14 01:43:51 +0800

    Action button triggers different actions

--- wxmp/components/ActionButton/ActionButton.js
+++ wxmp/components/ActionButton/ActionButton.js
@@ -4,7 +4,7 @@ Component({
    * Component properties
    */
   properties: {
-
+    action: String
   },
 
   /**
@@ -13,13 +13,33 @@ Component({
   data: {
 
   },
+  lifetimes: {
+    attached(){
+      console.log("properties", this.properties)
+    }
+  },
 
   /**
    * Component methods
    */
   methods: {
     takeAction(e){
-      console.log("Inside take action", e.currentTarget.dataset.action)
+      const action =  e.currentTarget.dataset.action
+      if(action == "bookClass"){
+        this.bookClass()
+      } else if(action == "queueUp"){
+        this.queueUp()
+      }
+
+    },
+    
+    bookClass(){
+      console.log("BOOK CLASS!!!")
+
+    },
+
+    queueUp(){
+      console.log("QUEUE UP!!")
     }
   }
 })
