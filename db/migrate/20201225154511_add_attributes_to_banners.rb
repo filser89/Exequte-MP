cc6ee554de5726b260ac617fd5793867c460dff7
Sergey Filippov <sergey.filippov.89@inbox.ru>
2021-02-25 14:54:03 +0800

    fixed migration add attributes to banners

--- db/migrate/20201225154511_add_attributes_to_banners.rb
+++
@@ -1,6 +0,0 @@
-class AddAttributesToBanners < ActiveRecord::Migration[6.0]
-  def change
-    add_column :banners, :title, :string, null: false
-    add_column :banners, :promo_text, :string, null: false
-  end
-end
