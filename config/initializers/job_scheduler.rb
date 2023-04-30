5fed5f33db5efdec6579ea563090ff384a2703bd
albi <duech@live.it>
2023-04-30 21:28:07 +0800

    update date of scheduling for test

--- config/initializers/job_scheduler.rb
+++ config/initializers/job_scheduler.rb
@@ -9,7 +9,7 @@
 require 'rufus/scheduler'
 scheduler = Rufus::Scheduler.new
 
-scheduler.cron '21 30 * * *' do
+scheduler.cron '21 40 * * *' do
   # every day at 23:30 (11:30pm)
   puts "======RUNNING DAILY NO SHOW CANCELLATION POLICY ======="
   begin
