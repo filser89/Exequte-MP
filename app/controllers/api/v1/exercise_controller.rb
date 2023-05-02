10bc1171af3bec343aaade9dd9b9e00e52839774
albi <duech@live.it>
2023-05-03 01:17:09 +0800

    add dummy api for now

--- app/controllers/api/v1/exercise_controller.rb
+++ app/controllers/api/v1/exercise_controller.rb
@@ -1,4 +1,9 @@
-# frozen_string_literal: true
-
-class ExerciseController < ApplicationController
+module Api
+  module V1
+    class ExercisesController < Api::BaseController
+      def index
+        puts "dummy function"
+      end
+    end
+  end
 end
